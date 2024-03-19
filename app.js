6cfd88023334fc4f50ec355e0d8f0e8d4ac664aa
Suresh Dahal <sd807141@gmail.com>
2024-03-19 17:35:41 +0545

    complete project

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const hamburger = document.querySelector('.hamburger');
 const ul = document.querySelector('ul');
 
+const menuItems = document.querySelectorAll('li');
+
+menuItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        ul.style.right = '-100%';
+    });
+});
+
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
 
@@ -10,3 +19,7 @@ hamburger.addEventListener('click', () => {
         ul.style.right = '-100%';
     }
 });
+
+const dateContainer = document.querySelector('.date');
+const date = new Date();
+dateContainer.textContent = date.getFullYear();
